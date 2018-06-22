ccf7e9e2db2a6b2dc2dcfd1ac79cc246f2bcf767
zach-256 <whitten.zach@gmail.com>
2018-06-22 16:43:47 -0400

    Added necessary columns and column selection menu

--- Movie Library/UI/NSTableView's/MovieDisplay.swift
+++ Movie Library/UI/NSTableView's/MovieDisplay.swift
@@ -16,7 +16,8 @@ class MovieDisplay: NSObject, NSTableViewDataSource, NSTableViewDelegate{
     
     //Called by AppDelegate after application has finished launching. Think of this function as an initalization function
     func viewDidLoad(){
-        
+        tableView.autosaveName = NSTableView.AutosaveName(rawValue: "MovieDisplayTableView")
+        tableView.autosaveTableColumns = true
     }
     
     func numberOfRows(in tableView: NSTableView) -> Int {
@@ -28,11 +29,12 @@ class MovieDisplay: NSObject, NSTableViewDataSource, NSTableViewDelegate{
         let identifier = tableColumn?.identifier.rawValue
         
         var cellView: NSTableCellView?
-        
+
         
         if identifier == "title" {
             cellView = (tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "TitleCell"), owner: nil) as? NSTableCellView)!
             cellView!.textField?.stringValue = movieData[row].title ?? ""
+            
         }
         
     
@@ -48,4 +50,45 @@ class MovieDisplay: NSObject, NSTableViewDataSource, NSTableViewDelegate{
             //TODO enable edit and delete menu items 
         }
     }
+    
+   
+    
+    
+    @IBAction func TableViewMenuItem(_ sender: Any){
+        let menuItem = (sender as! NSMenuItem)
+        
+        menuItem.state == .off ? (menuItem.state = .on) : (menuItem.state = .off)
+        
+        
+        var tableColumn: NSTableColumn? = nil
+        if menuItem.title == "Aspect Ratio" { tableColumn = tableView.tableColumn(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "aspectRatio")) }
+        if menuItem.title == "Bit Rate" { tableColumn = tableView.tableColumn(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "bitRate")) }
+        if menuItem.title == "Comments" { tableColumn = tableView.tableColumn(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "comments")) }
+        if menuItem.title == "Director" { tableColumn = tableView.tableColumn(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "director")) }
+        if menuItem.title == "Frame Rate" { tableColumn = tableView.tableColumn(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "frameRate")) }
+        if menuItem.title == "Genre" { tableColumn = tableView.tableColumn(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "genre")) }
+        if menuItem.title == "Height" { tableColumn = tableView.tableColumn(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "height")) }
+        if menuItem.title == "Play Count" { tableColumn = tableView.tableColumn(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "playCount")) }
+        if menuItem.title == "Run Time" { tableColumn = tableView.tableColumn(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "runTime")) }
+        if menuItem.title == "Size" { tableColumn = tableView.tableColumn(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "size")) }
+        if menuItem.title == "Title" { tableColumn = tableView.tableColumn(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "title")) }
+        if menuItem.title == "Video Media" { tableColumn = tableView.tableColumn(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "videoMedia")) }
+        if menuItem.title == "Width" { tableColumn = tableView.tableColumn(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "width")) }
+        if menuItem.title == "Year" { tableColumn = tableView.tableColumn(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "year")) }
+        
+        menuItem.state == .off ? () : ()
+        if(menuItem.state == .off){
+            tableColumn?.isHidden = true
+            tableColumn?.width = 0
+            tableColumn?.minWidth = 0
+        }
+        else{
+            tableColumn?.isHidden = false
+            tableColumn?.width = 50
+            tableColumn?.minWidth = 10
+        }
+        
+
+    }
+    
 }
